d591e69a5bdeb56391c04388d23168a11de5680c
fi11 <pavelsilinw@gmail.com>
2017-06-14 14:33:12 +0300

    add buildShadowBorder

--- src/View/story.jsx
+++ src/View/story.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import View from './View';
-import { StyleSheet, styled }from '../styles';
+import { StyleSheet, styled, buildShadowBorder }from '../styles';
 
 const st = StyleSheet.create({
   blackSquare: {
@@ -27,6 +27,37 @@ const BlackSquare = styled(View, {
 
 const RedSquare = styled(BlackSquare, {
   backgroundColor: 'red',
+});
+
+const RedSquareWithBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('3', '#333'),
+});
+
+const RedSquareWithOuterBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('3', '#333', true),
+});
+
+const RedSquareWithTopBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('3 0 0 0', '#333'),
+});
+
+const RedSquareWithBottomBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('0 0 3 0', '#333'),
+});
+
+const RedSquareWithTopAndBottomBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('3 0 3', '#333'),
+});
+
+const RedSquareWithRightBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('0 3 0 0', '#333'),
+});
+
+const RedSquareWithLeftBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('0 0 0 3', '#333'),
+});
+const RedSquareWithRightAndLeftBorder = styled(RedSquare, {
+  boxShadow: buildShadowBorder('0 3 0', '#333'),
 });
 
 storiesOf('View', module)
@@ -55,4 +86,28 @@ storiesOf('View', module)
   ))
   .add('styled extend', () => (
     <RedSquare />
+  ))
+  .add('with border as shadow', () => (
+    <RedSquareWithBorder />
+  ))
+  .add('with outer border as shadow', () => (
+    <RedSquareWithOuterBorder />
+  ))
+  .add('with top border as shadow', () => (
+    <RedSquareWithTopBorder />
+  ))
+  .add('with bottom border as shadow', () => (
+    <RedSquareWithBottomBorder />
+  ))
+  .add('with top and bottom border as shadow', () => (
+    <RedSquareWithTopAndBottomBorder />
+  ))
+  .add('with right border as shadow', () => (
+    <RedSquareWithRightBorder />
+  ))
+  .add('with left border as shadow', () => (
+    <RedSquareWithLeftBorder />
+  ))
+  .add('with right and left border as shadow', () => (
+    <RedSquareWithRightAndLeftBorder />
   ));
--- src/styles.js
+++ src/styles.js
@@ -8,4 +8,58 @@ const styled = (element, styleRules) => {
   return styleRules === undefined ? _styled(View, styleRules) : _styled(element, styleRules);
 };
 
-export { StyleSheet, cn, styled };
+const addPXSuffix = (value, isNegative) => {
+  return +value ? `${isNegative ? '-' : ''}${value}px` : 0;
+};
+
+const buildShadowBorder = (borderWidth, borderColor, out) => {
+  const inset = out ? '' : 'inset ';
+  const split = String(borderWidth).replace(/px/g, '').split(' ').map(item => +item);
+  const length = split.length;
+  let [topWidth, rightWidth, bottomWidth, leftWidth] = split;
+
+  if (length === 1) {
+    rightWidth = topWidth;
+    bottomWidth = topWidth;
+    leftWidth = topWidth;
+
+  } else if (length === 2) {
+    bottomWidth = topWidth;
+    leftWidth = rightWidth;
+
+  } else if (length === 3 ) {
+    leftWidth = rightWidth;
+  }
+
+  if (topWidth === rightWidth && topWidth === bottomWidth && topWidth === leftWidth) {
+    return topWidth ? `${inset}0 0 0 ${addPXSuffix(topWidth)} ${borderColor}` : 'none';
+  }
+
+  let topAndLeft = '';
+  let bottomAndRight = '';
+
+  if (topWidth && leftWidth) {
+    topAndLeft = `${inset}${addPXSuffix(leftWidth)} ${addPXSuffix(topWidth)} 0 0 ${borderColor}`;
+  } else if (topWidth) {
+    topAndLeft = `${inset}0 ${addPXSuffix(topWidth)} 0 0 ${borderColor}`;
+  } else if (leftWidth) {
+    topAndLeft = `${inset}${addPXSuffix(leftWidth)} 0 0 0 ${borderColor}`;
+  }
+
+  if (bottomWidth && rightWidth) {
+    bottomAndRight = `${inset}${addPXSuffix(rightWidth, true)} ${addPXSuffix(bottomWidth, true)} 0 0 ${borderColor}`;
+  } else if (bottomWidth) {
+    bottomAndRight = `${inset}0 ${addPXSuffix(bottomWidth, true)} 0 0 ${borderColor}`;
+  } else if (rightWidth) {
+    bottomAndRight = `${inset}${addPXSuffix(rightWidth, true)} 0 0 0 ${borderColor}`;
+  }
+
+  let result = [];
+
+  topAndLeft && result.push(topAndLeft);
+  bottomAndRight && result.push(bottomAndRight);
+
+  return result.join(',');
+};
+
+export { StyleSheet, cn, styled, addPXSuffix, buildShadowBorder };
